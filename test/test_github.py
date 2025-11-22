602c1fb2c842d035950601c347e8e80b4ef56cc2
Gran <awebdeff@gmail.com>
2025-11-22 15:44:51 +0300

    add annotations

--- test/test_github.py
+++ test/test_github.py
@@ -1,39 +1,26 @@
-import time
-
 import pytest
 from selenium import webdriver
-
-
-@pytest.mark.github
-def test_github():
-    driver = webdriver.Chrome()
-    URL = "https://github.com/"
-    driver.get(URL)
-
-    time.sleep(2)  # чтобы браузер не сразу закрывался
-
-    assert driver.current_url == URL
-    assert "GitHub" in driver.title
-
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.remote.webdriver import WebDriver
+from typing import Generator
+
 
 @pytest.fixture
-def driver():
+def driver() -> Generator[WebDriver, None, None]:
     opts = Options()
-
     opts.add_argument("--headless=new")
     opts.add_argument("--window-size=1280,900")
+
     driver = webdriver.Chrome(options=opts)
     yield driver
 
     driver.quit()
 
 
-def test_selenium_web(driver):
-    url = "https://www.selenium.dev/"
+@pytest.mark.github
+def test_github(driver: WebDriver) -> None:
+    url = "https://github.com/"
     driver.get(url)
-    assert driver.title == "Selenium"
+
+    assert "GitHub" in driver.title
     assert driver.current_url == url
