f9f3f0ced842e598ae2506d772a6e5e4785daa81
Gran <awebdeff@gmail.com>
2025-12-21 12:09:43 +0300

    new common repo for all hw

---
+++ hw_3/codefix.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+
+def greet(name):
+    print("Привет, " + name)
+
+
+if __name__ == "__main__":
+    greet("мир")
+    print(add(2, 2))
---
+++ hw_5/README.md
@@ -0,0 +1,2 @@
+## Homework 5 for QAGURU 23
+
